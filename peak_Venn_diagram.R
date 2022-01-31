fcfa1435e7b583ff203e4ab1c4360f47abad9923
Lie-ne <68026087+Lie-ne@users.noreply.github.com>
2022-01-31 17:41:03 +0100

    Visualise overlapping peak counts from narrowPeak files

---
+++ peak_Venn_diagram.R
@@ -0,0 +1,22 @@
+# Import the libraries
+library(rtracklayer)
+library(ChIPpeakAnno)
+
+# Set the extraCols for narrowPeak
+extraCols_narrowPeak <- c(signalValue = "numeric", pValue = "numeric",
+                          qValue = "numeric", peak = "integer")
+
+# Create the GRanges files for your replicates
+FLAG_rep1 <- import('FLAG-ChIP_rep1.narrowPeak', format = "BED",
+                       extraCols = extraCols_narrowPeak)
+
+FLAG_rep2_gr <- import('FLAG-ChIP_rep2.narrowPeak', format = "BED",
+                       extraCols = extraCols_narrowPeak)
+
+# Find the overlaps
+ol <- findOverlapsOfPeaks(FLAG_rep2, FLAG_rep1)
+
+# Make Venn diagram
+makeVennDiagram(ol, fill=c("pink", "blue"), # circle fill color
+                col=c("black", "black"), #circle border color
+                cat.col=c("black", "black")) # label color, keep same as circle border color
